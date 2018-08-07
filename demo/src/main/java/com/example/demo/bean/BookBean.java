66470f3bc6cb78f26e7977a30d31ebab6783c261
songliqing <songliqing@yto.net.cn>
2018-08-07 09:02:57 +0800

    first commit

---
+++ demo/src/main/java/com/example/demo/bean/BookBean.java
@@ -0,0 +1,41 @@
+package com.example.demo.bean;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by admin on 2018/8/6.
+ */
+@Component
+@ConfigurationProperties(prefix = "book")
+@PropertySource("classpath:book.properties")//指定配置文件位置
+public class BookBean {
+    private String name;
+    private String author;
+    private String pinyin;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPinyin() {
+        return pinyin;
+    }
+
+    public void setPinyin(String pinyin) {
+        this.pinyin = pinyin;
+    }
+}
